339bb5fc18e0e94d0abf53629fe481d88691b24f
alfonsog0 <alfonsogambino@protonmail.com>
2025-02-11 22:58:38 +0000

    added db url and '/' route

--- movies.py
+++ movies.py
@@ -11,6 +11,10 @@ def get_db_connection():
     """Creates a new database connection"""
     return psycopg2.connect(DATABASE_URL)
 
+@app.route("/")
+def home():
+    return "Movie API is running!"
+
 # ✅ Route: Get movies with pagination
 ## pagination example: http://127.0.0.1/movies?page=1&per_page=1000
 @app.route("/movies", methods=["GET"])
