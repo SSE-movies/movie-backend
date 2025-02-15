d05390d45ec8a76bb301e6a23fc89c8ad8c37e43
Tiago <tiagoriely@gmail.com>
2025-02-15 23:33:18 +0000

    switched from railways to supabase + added build.yml

--- movies.py
+++ movies.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify, request
+from dotenv import load_dotenv
 import psycopg2
 import os
 
 app = Flask(__name__)
+load_dotenv()
 
 # Connect to PostgreSQL
 DATABASE_URL = os.getenv("DATABASE_URL")
@@ -29,7 +31,7 @@ def get_movies():
 
     # ✅ Fetch paginated results
     cur.execute('''
-        SELECT show_id, type, title, director, "cast", country, date_added, release_year, rating, duration, listed_in, description 
+        SELECT "showId", type, title, director, "cast", country, "dateAdded", "releaseYear", rating, duration, "listedIn", description 
         FROM movies
         LIMIT %s OFFSET %s;
     ''', (per_page, offset))
@@ -42,17 +44,17 @@ def get_movies():
     # ✅ Convert query results to JSON format
     movies_list = [
         {
-            "show_id": row[0],
+            "showId": row[0],
             "type": row[1],
             "title": row[2],
             "director": row[3],
             "cast": row[4],
             "country": row[5],
             "date_added": row[6],
-            "release_year": row[7],
+            "releaseYear": row[7],
             "rating": row[8],
             "duration": row[9],
-            "listed_in": row[10],
+            "listedIn": row[10],
             "description": row[11],
         }
         for row in movies
